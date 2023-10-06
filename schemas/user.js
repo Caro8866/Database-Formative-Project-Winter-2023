0ea430863670e02a3d1f50af5d9ab3db1a9667e4
Caroline Thostrup <caro342h@stud.kea.dk>
2023-10-06 14:05:55 +0200

    update schemas

--- schemas/user.js
+++ schemas/user.js
@@ -4,8 +4,8 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true, minlength: 8 },
   email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, minlength: 8 },
   TopicID: { type: mongoose.Schema.Types.ObjectId, ref: "Topic" },
 });
 
--- schemas/userActivity.js
+++ schemas/userActivity.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 const UserActivitySchema = new mongoose.Schema({
   UserID: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   ActivityID: { type: mongoose.Schema.Types.ObjectId, ref: "Activity", required: true },
-  status: { type: Boolean, required: true },
+  status: { type: String, required: true }, // In Progress, Completed, Not Started
   score: { type: Number },
-  completedTime: { type: Date },
+  completedTimestamp: { type: Date },
 });
 
 UserActivitySchema.index({ UserID: 1, ActivityID: 1 }, { unique: true });
