ad952ec7709831f70c12dc4f9df10b67b4c1a04a
Caroline Thostrup <caro342h@stud.kea.dk>
2023-10-07 19:02:09 +0200

    adjust display activitie and add display resources

--- scripts/topic.js
+++ scripts/topic.js
@@ -2,6 +2,14 @@ document.addEventListener("DOMContentLoaded", () => {
   const urlParas = new URLSearchParams(window.location.search);
   const topicId = urlParas.get("id");
 
+  fetch(`http://localhost:3000/topics/${topicId}`)
+    .then((response) => response.json())
+    .then((topic) => {
+      document.querySelector(".topic-title").textContent = topic.title;
+      document.querySelector(".topic-description").textContent = topic.description;
+      displayResources(topic.resources);
+    });
+
   fetch(`http://localhost:3000/activities/topic/${topicId}`)
     .then((response) => response.json())
     .then((activities) => {
@@ -10,21 +18,72 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function displayActivities(activities) {
     const activitiesList = document.querySelector(".activities ul");
+    let hasActivities = false;
 
-    // Remove any existing list items
     while (activitiesList.firstChild) {
       activitiesList.removeChild(activitiesList.firstChild);
     }
 
     activities.forEach((activity) => {
+      if (activity) {
+        hasActivities = true;
+        const listItem = document.createElement("li");
+        const link = document.createElement("a");
+
+        link.textContent = activity.name;
+        link.href = `activity.html?id=${activity._id}`;
+
+        listItem.appendChild(link);
+        activitiesList.appendChild(listItem);
+      }
+    });
+
+    if (!hasActivities) {
       const listItem = document.createElement("li");
       const link = document.createElement("a");
 
-      link.textContent = activity.name;
-      link.href = `activity.html?id=${activity._id}`; // Create a link to the activity's dedicated page, if you have one
+      link.textContent = "No activities available.";
+      link.href = "#";
+
+      link.style.pointerEvents = "none";
 
       listItem.appendChild(link);
       activitiesList.appendChild(listItem);
-    });
+    }
+  }
+
+  function displayResources(resources) {
+    const resourcesList = document.querySelector(".resources ul");
+    let hasResources = false;
+
+    while (resourcesList.firstChild) {
+      resourcesList.removeChild(resourcesList.firstChild);
+    }
+
+    if (resources) {
+      hasResources = true;
+      resources.forEach((resource) => {
+        const listItem = document.createElement("li");
+        const link = document.createElement("a");
+
+        link.textContent = resource.name || resource.url;
+        link.href = resource.url;
+
+        listItem.appendChild(link);
+        resourcesList.appendChild(listItem);
+      });
+    }
+
+    if (!hasResources) {
+      const listItem = document.createElement("li");
+      const link = document.createElement("a");
+
+      link.textContent = "No resources available.";
+      link.href = "#";
+      link.style.pointerEvents = "none";
+
+      listItem.appendChild(link);
+      resourcesList.appendChild(listItem);
+    }
   }
 });
